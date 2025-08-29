20e5762bbbafe1eda2e99428b74900729d15a8ed
Wool5443 <mihsolodilov2015@gmail.com>
2025-08-30 00:54:36 +0300

    Upd
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- ShkolkovoCreate/build_tree.py
+++ ShkolkovoCreate/build_tree.py
@@ -1,4 +1,5 @@
 from itertools import product
+from sys import argv
 
 
 def check_good_key(key_vals, key):
@@ -27,8 +28,14 @@ OUTRO = \
 }
     """
 
+if len(argv) != 2:
+    print(f"Usage: {argv[0]} <output_file>")
+    exit(-1)
+
 print("Введите <код> <буква> построчно. Когда закончите, введите\
       пустую строку")
+
+result_file = open(argv[1], "w")
 
 levels = 0
 key_vals = {}
@@ -38,7 +45,6 @@ while s:
     key_vals[key_val[0]] = key_val[1]
     levels = max(len(key_val[0]), levels)
     s = input()
-print(levels)
 
 keys = []
 invisible_keys = []
@@ -50,30 +56,32 @@ for i in range(1, levels + 1):
         if not check_good_key(key_vals, key):
             invisible_keys.append(key)
 
-print(INTRO)
+print(INTRO, file=result_file)
 
 for k in keys:
     val = ""
     if k in key_vals:
-        print(f"    t{k}[label = \"{{{k}|{key_vals[k]}}}\"];")
+        print(f"    t{k}[label = \"{{{k}|{key_vals[k]}}}\"];", file=result_file)
     elif k in invisible_keys:
-        print(f"    t{k}[style = invis];")
+        print(f"    t{k}[style = invis];", file=result_file)
     else:
-        print(f"    t{k}[label = \"{{{k}}}\"];")
+        print(f"    t{k}[label = \"{{{k}}}\"];", file=result_file)
 
 
-print("    root->{t0, t1}")
+print("    root->{t0, t1}", file=result_file)
 
 for k in keys[:2 ** levels - 2]:
     key0 = f"{k}0"
     key1 = f"{k}1"
-    print(f"    t{k}->t{k}0", end="")
+    print(f"    t{k}->t{k}0", end="", file=result_file)
     if key0 in invisible_keys:
-        print("[style = invis]", end="")
-    print(";")
-    print(f"    t{k}->t{k}1", end="")
+        print("[style = invis]", end="", file=result_file)
+    print(";", file=result_file)
+    print(f"    t{k}->t{k}1", end="", file=result_file)
     if key1 in invisible_keys:
-        print("[style = invis]", end="")
-    print(";")
+        print("[style = invis]", end="", file=result_file)
+    print(";", file=result_file)
 
-print(OUTRO)
+print(OUTRO, file=result_file)
+
+result_file.close()
